a7f1671568d17ead2b575ee16fdd1bfdeefa6822
midnight5un <landmanlandis41@gmail.com>
2016-01-24 18:34:04 -0500

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two independent functions listed in this script; the first will take some
+## input and return a matrix of the input, also while finding and storing the
+## inverse of said matrix in "z". The second function will look for the inverse
+## of the created matrix and if none is found will compute and return the 
+## initially sought after inverse.
 
-## Write a short comment describing this function
+## Creates function from input and saves the inverse in "z"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        slv<- NULL
+        set<-function(y){
+                x<<-y
+                slv<<-NULL
+        }
+        get<-function()x
+        setslv<-function(solv)slv<<-solv
+        getslv<-function()slv
+        list(set=set,get=get,setslv=setslv,getslv=getslv)
 }
 
 
-## Write a short comment describing this function
+## Attempts to return a saved value of "z" being the inverse of a matrix.
+## If no value of "z" is found, this function will compute and return the
+## inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        slv<-x$getslv()
+        if(!is.null(slv)){
+                print("Abra Kadabra")
+                return(slv)
+        }
+        m<-x$get()
+        slv<-solve(m,...)
+        x$setslv(slv)
+        slv
 }
